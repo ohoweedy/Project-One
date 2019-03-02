399e8b28e701b1ac3a4d9b7462b1448205f12012
ohoweedy <howeedy5@gmail.com>
2019-03-02 14:45:05 -0500

    saturday update omar

--- project_one_api.py
+++ project_one_api.py
@@ -1,2 +1,10 @@
-# OpenWeatherMap API Key
-api_key = "y2pgcdahaqn4fcf3yym9v98a"
+# MLB API Key
+mlb_api_key = "y2pgcdahaqn4fcf3yym9v98a"
+
+# NFL API Key
+nfl_api_key = "w3zwxzkx89h2ptq9akddjgfa"
+
+# NHL API Key
+nhl_api_key = "48pzd6rpf5ttz5ua3gxfayyh"
+
+
